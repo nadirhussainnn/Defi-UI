a1ef3d8384162a257936821f11d97d6db2212330
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-07-28 17:11:36 +0500

    added network and fetched history and balance

--- src/components/BalanceList.js
+++ src/components/BalanceList.js
@@ -3,68 +3,53 @@
  * @version 1.0
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import eth_ic from "../assets/images/ethereum.png";
 import weth_ic from "../assets/images/weth.png";
 
-export default function BalanceList({ address }) {
+export default function BalanceList({ address, usd_rate, provider }) {
+  
+  const [history, setHistory] = useState(null);
 
-  address = "0x1dD1619Ca2632292aE6238E5332043B29EFB7DC8"; //This address is expected from metamask wallet
+  useEffect(() => {
 
-  const data = [
-    {
-      symbol: "ETH",
-      name: "Ethereum",
-      icon: eth_ic,
-      ethers: "1.01",
-      usd: "1000",
-    },
-    {
-      symbol: "WETH",
-      name: "Wrapped Ethereum",
-      icon: weth_ic,
-      ethers: "1.01",
-      usd: "1000",
-    },
-    {
-      symbol: "WETH",
-      name: "Wrapped Ethereum",
-      icon: weth_ic,
-      ethers: "1.01",
-      usd: "1000",
-    },
-    {
-      symbol: "ETH",
-      name: "Ethereum",
-      icon: eth_ic,
-      ethers: "1.01",
-      usd: "1000",
-    },
-  ];
+    //Get transaction history and balance to this address
+    const historyFetcher = async () => {
+      let ropstonProvider = new ethers.providers.EtherscanProvider(
+        provider?.name
+      );
+      let history = await ropstonProvider.getHistory(address);
+      setHistory(history);
+    };
+    historyFetcher();
+  }, []);
+
   return (
     <>
-      {data.map((item) => {
-        return <Item item={item} />;
+      {history?.map((item, index) => {
+        const eth = ethers.utils.formatEther(item.value._hex);
+        return <Item eth={eth} usd_rate={usd_rate} key={index} />;
       })}
     </>
   );
 }
 
-const Item = ({ item }) => {
+const Item = ({ eth, usd_rate }) => {
+  const usd = Math.round((usd_rate * eth + Number.EPSILON) * 100) / 100;
+
   return (
     <div style={{ display: "flex", marginTop: "10px" }}>
       <div style={{ display: "flex" }}>
-        <img src={item.icon} width={40} height={40} alt="icon"></img>
+        <img src={eth_ic} width={40} height={40} alt="icon" />
         <div style={{ marginLeft: "15px" }}>
-          <h3 style={{ fontWeight: "bold", textAlign: "left" }}>
-            {item.symbol}
-          </h3>
-          <h5 style={{ left: "10px", marginTop: "-10px" }}>{item.name}</h5>
+          <h3 style={{ fontWeight: "bold", textAlign: "left" }}>{"ETH"}</h3>
+          <h5 style={{ left: "10px", marginTop: "-10px" }}>{"Ethereum"}</h5>
         </div>
       </div>
       <div style={{ position: "absolute", right: "20px" }}>
-        <h3 style={{ left: 0, fontWeight: "bold" }}>{item.ethers}</h3>
-        <h5 style={{ marginTop: "-10px" }}>${item.usd}</h5>
+        <h3 style={{ left: 0, fontWeight: "bold" }}>{eth}</h3>
+        <h5 style={{ marginTop: "-10px" }}>${usd}</h5>
       </div>
     </div>
   );
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -4,15 +4,17 @@
  */
 
 import React, { useState } from "react";
-import { Button, Modal, Space, Divider, Select } from "antd";
+import { Button, Modal, Space, Divider, Select, Input } from "antd";
 
 import metamask_ic from "../assets/images/metamask.png";
 import { ethers } from "ethers";
 import { MdCircle } from "react-icons/md";
 import { IoMdCopy, IoMdEye, IoMdOpen } from "react-icons/io";
+import { RiMoneyDollarCircleFill, RiUserReceived2Fill } from "react-icons/ri";
 import { toHex, truncateAddress } from "../utils/utils";
 import { networkParams } from "../utils/networks";
 import BalanceList from "./BalanceList";
+import { useFormik } from "formik";
 
 import "../styles/styles.css";
 
@@ -26,7 +28,35 @@ export default function Wallet() {
   const [provider, setProvider] = useState(null);
   const [balance, setBalance] = useState(0);
   const [account, setAccount] = useState();
+  const [usdRate, setUsdRate] = useState(0);
+  const [network, setNetwork] = useState();
+  const [sendCryptoModal, setSendCryptoModal] = useState(false);
 
+  const formik = useFormik({
+    initialValues: {
+      receiver: "",
+      amount: "",
+    },
+
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.receiver) {
+        errors.receiver = "Address required";
+      }
+      if (!values.amount) {
+        errors.amount = "Amount required";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+        //Sign transaction and send amount
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = await provider.getSigner();
+        console.log(provider)
+    },
+  });
   //Helper functions
 
   async function connectWallet() {
@@ -37,12 +67,31 @@ export default function Wallet() {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = await provider.getSigner();
         const currentAddress = await signer.getAddress();
-        const balance = await signer.getBalance();
+        let balance = await ethers.utils.formatEther(await signer.getBalance());
 
+        //Perform ETH to USD conversion
+        const currencyRate = await (
+          await fetch(
+            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
+          )
+        ).json();
+        balance =
+          Math.round(
+            (parseFloat(currencyRate.USD) * balance + Number.EPSILON) * 100
+          ) / 100;
+
+        //Get network info
+        const network = await provider.getNetwork();
+
+        //Set States
+        setNetwork(network);
+        setUsdRate(parseFloat(currencyRate.USD));
         setBalance(balance);
         setAccount(currentAddress);
         setProvider(provider);
         setVisible(true);
+      } else {
+        alert(`You don't have Metamask Installed on device`);
       }
     } catch (error) {
       console.error(error);
@@ -117,27 +166,35 @@ export default function Wallet() {
           closable={false}
           className="modal-bg"
         >
-          <div
-            style={{
-              display: "flex",
-              position: "absolute",
-              marginTop: "-10px",
-              right: 20,
-            }}
-          >
-            <MdCircle color={account ? "#01aa58" : "red"} />
-            <span
+          <div>
+            <div style={{ display: "flex" }}>
+              <span style={{}}>
+                Network: <b>{network?.name?.toUpperCase()}</b>
+              </span>
+            </div>
+            <div
               style={{
-                color: account ? "#01aa58" : "red",
-                marginTop: "-6px",
-                marginLeft: "3px",
+                display: "flex",
+                position: "absolute",
+                marginTop: "-15px",
+                right: 20,
               }}
             >
-              {account ? "connected" : "Not connected"}
-            </span>
+              <MdCircle color={account ? "#01aa58" : "red"} />
+              <span
+                style={{
+                  color: account ? "#01aa58" : "red",
+                  marginTop: "-6px",
+                  marginLeft: "3px",
+                }}
+              >
+                {account ? "connected" : "Not connected"}
+              </span>
+            </div>
           </div>
+
           <Space direction="vertical" />
-          <Divider className="margin-top-10 antd-divider" />
+          <Divider className="antd-divider" />
 
           {account ? (
             <div>
@@ -168,8 +225,8 @@ export default function Wallet() {
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
                   <span style={{ fontSize: 30 }}>
-                    {/* <b>${balance.toString()}</b> */}
-                    <b>$4000 </b>
+                    <b>${balance.toString()}</b>
+                    {/* <b>$4000 </b> */}
                   </span>
                   <IoMdEye
                     size={30}
@@ -183,16 +240,14 @@ export default function Wallet() {
                     marginTop: "20px",
                   }}
                 >
-                  <Button
-                    style={{
-                      backgroundColor: "#01aa58",
-                      border: "none",
-                      color: "white",
-                      fontSize: "16px",
-                      borderRadius: "5px",
-                    }}
-                    onClick={buyCrypto}
-                  >
+                  <Button style={{
+                    backgroundColor: "#01aa58",
+                    border: "none",
+                    color: "white",
+                    fontSize: "16px",
+                    marginLeft: "10px",
+                    borderRadius: "5px",
+                  }} onClick={buyCrypto}>
                     Buy
                   </Button>
                   <Button
@@ -204,12 +259,18 @@ export default function Wallet() {
                       marginLeft: "10px",
                       borderRadius: "5px",
                     }}
-                    onClick={sendCrypto}
+                    onClick={() => {
+                      setSendCryptoModal(true);
+                    }}
                   >
                     Send
                   </Button>
                 </div>
-                <BalanceList address={account} />
+                <BalanceList
+                  address={account}
+                  usd_rate={usdRate}
+                  provider={network}
+                />
               </div>
             </div>
           ) : (
@@ -241,13 +302,66 @@ export default function Wallet() {
             </Select>
           </div>
         </Modal>
+
+        {/* Modal to send assets */}
+
+        <Modal
+          centered
+          title="Send Money"
+          visible={sendCryptoModal}
+          onCancel={()=>{setSendCryptoModal(false)}}
+          width={"30%"}
+          footer={null}
+          closable={true}
+        >
+          <form onSubmit={formik.handleSubmit}>
+            <div style={{ justifyContent: "center" }}>
+              <div
+                style={{
+                  padding: "10px",
+                }}
+              >
+                <Input
+                  size="large"
+                  name="receiver"
+                  placeholder="Enter Receiver public address"
+                  prefix={<RiUserReceived2Fill />}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              </div>
+
+              <div
+                style={{
+                  padding: "10px",
+                }}
+              >
+                <Input
+                  size="large"
+                  name="amount"
+                  placeholder="Enter amount in USD"
+                  prefix={<RiMoneyDollarCircleFill />}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              </div>
+              <div style={{ display: "flex", justifyContent:'end', marginRight:'10px' }}>
+                <button type="submit" style={{backgroundColor: "#01aa58",
+                      border: "none",
+                      color: "white",
+                      fontSize: "16px",
+                      marginLeft: "10px",
+                      borderRadius: "5px",
+                      padding:'5px',
+                      cursor:'pointer'
+                      }}>
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </form>
+        </Modal>
       </div>
     </div>
   );
 }
-
-// const block=await provider.getBlockNumber()
-// let ropstonProvider = new ethers.providers.EtherscanProvider("ropsten");
-// let history = await ropstonProvider.getHistory(
-//   provider.provider.selectedAddress
-// );
